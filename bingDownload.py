7224da652d1b822c10906608c0fde64e7db82306
Henry Li <xl562@cam.ac.uk>
2023-02-13 18:50:03 +0000

    Trying to keep up with Bing site update but failed

--- bingDownload.py
+++ bingDownload.py
@@ -1,21 +1,26 @@
 '''
 exif
 '''
-from urllib import response
 from lxml import etree
-from matplotlib.streamplot import interpgrid
 import requests
 
 url = 'https://www.bing.com'
 head = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.47'}
-resp = requests.get(url, headers = head).content.decode('utf-8')
-forbiddenChars = ['\\','/',':','*','?','"','<','>','|']  #9 forbidden chars in WinOS file system
+resp = requests.get(url, headers = head)#.content.decode('utf-8')
+forbiddenChars = ['\\','/',':','*','?','"','<','>','|']  # 9 forbidden chars in WinOS file system
 replacmntChars = ['＼','／','：','＊','？','＂','＜','＞','｜']
 
-e = etree.HTML(resp)
+
+root = etree.HTML(resp.text)
+print(root.xpath("/html"))
+
+
+assert 0
+
 
 #getting the file name aka title of the image
-imgTitle1_asList = e.xpath('//div[@class="musCardCont"]/h2/a/text()')
+imgTitle1_asList = e.xpath("//div[@class='musCardCont']/h2/a/text()")
+print(imgTitle1_asList)
 imgTitle1 = str(imgTitle1_asList[0])
 imgTitle2_asList = e.xpath('//div[@class="icon_text"]/a/h1/text()')
 imgTitle2 = str(imgTitle2_asList[0])
@@ -24,8 +29,9 @@ for i in range(8):
     imgTitle = imgTitle.replace(forbiddenChars[i],replacmntChars[i])
 
 #getting the link to the image
-imgLinkLocation = e.xpath('/html/head/link[@rel="preload"]')
-imgLink = imgLinkLocation[0].attrib.get('href')
+imgLinkLocation = e.xpath('//div[@class="img_uhd]"')
+imgLink = imgLinkLocation[0].attrib.get('style')
+print('====',imgLink,'====')
 imgLink = 'https://www.bing.com'+imgLink
 print(imgLink)
 
